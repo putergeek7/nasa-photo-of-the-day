8fbf20b7cb71b5afa156bbc55ddbc194359e9558
Pat D <49631522+putergeek7@users.noreply.github.com>
2020-01-23 06:26:35 -0800

    I am submitting what I have added so far

---
+++ components/PhotoCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// I still don't know
+
+const PhotoCard = props => {
+	return (
+		<div>
+			<h1>{props.title}</h1>
+			<h2>{props.date}</h2>
+			<img src={props.url} alt='Nasa Image' />
+			<div>
+				<h3>Details</h3>
+				<p>{props.explanation}</p>
+			</div>
+		</div>
+	);
+};
+
+export default PhotoCard;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import PhotoCard from './Components/PhotoCard';
+
+// I don't know what i am doing....
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+	const [nasaImg, setNasaImg] = useState([]);
+	useEffect(() => {
+		axios
+			.get(
+				'https://api.nasa.gov/planetary/apod?api_key=HeJ9yb3WaxSY1faBvrEJLfH4brmlYkRaYxo3eOPK'
+			)
+			.then(res => {
+				console.log(res.data);
+				setNasaImg(res.data);
+			});
+	}, []);
+
+	return (
+		<div className='App'>
+			<header>
+				<h1>NASA PHOTO OF THE DAY</h1>
+			</header>
+			<PhotoCard
+				title={nasaImg.title}
+				url={nasaImg.url}
+				explanation={nasaImg.explanation}
+				date={nasaImg.date}
+			/>
+		</div>
+	);
 }
 
 export default App;
